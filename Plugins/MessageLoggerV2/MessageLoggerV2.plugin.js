c248b434bca50caad74704802607d7fe015afb56
1Lighty <49841131+1Lighty@users.noreply.github.com>
2021-07-21 14:43:30 +0200

    Fuck @NYRI4

--- Plugins/MessageLoggerV2/MessageLoggerV2.plugin.js
+++ Plugins/MessageLoggerV2/MessageLoggerV2.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name MessageLoggerV2
- * @version 1.7.71
+ * @version 1.7.72
  * @invite NYvWdN5
  * @donate https://paypal.me/lighty13
  * @website https://1lighty.github.io/BetterDiscordStuff/?plugin=MessageLoggerV2
@@ -37,7 +37,7 @@ module.exports = class MessageLoggerV2 {
     return 'MessageLoggerV2';
   }
   getVersion() {
-    return '1.7.71';
+    return '1.7.72';
   }
   getAuthor() {
     return 'Lighty';
@@ -177,9 +177,9 @@ module.exports = class MessageLoggerV2 {
   getChanges() {
     return [
       {
-        title: '#justblamezeretf',
+        title: 'no longer welcome',
         type: 'fixed',
-        items: ['Fixed some misc styling issues.']
+        items: ['Nyria#3863 is attempting to force people to not use plugins they do not like. They do not have the power to control what plugins a person can or cannot use.', 'Do not support Nyria#3863 in any way (via donations or contributions, they are scum).', 'Nyria#3863 is trying to spread lies and misinformation, with absolutely no proof to backup their claims, the claims of which have been disproven many times before already.', 'Fixed Nyria#3863 being a piece of shit and blocking loggers via a theme.']
       }
     ];
   }
@@ -861,7 +861,7 @@ module.exports = class MessageLoggerV2 {
     const iconShit = ZeresPluginLibrary.WebpackModules.getByProps('container', 'children', 'toolbar', 'iconWrapper');
     // Icon by font awesome
     // https://fontawesome.com/license
-    this.channelLogButton = this.parseHTML(`<div tabindex="0" class="${iconShit.iconWrapper} ${iconShit.clickable}" id="${this.style.openLogs}" role="button">
+    this.channelLogButton = this.parseHTML(`<div tabindex="0" class="${iconShit.iconWrapper} ${iconShit.clickable}" role="button">
                                                         <svg aria-hidden="true" class="${iconShit.icon}" name="Open Logs" viewBox="0 0 576 512">
                                                             <path fill="currentColor" d="M218.17 424.14c-2.95-5.92-8.09-6.52-10.17-6.52s-7.22.59-10.02 6.19l-7.67 15.34c-6.37 12.78-25.03 11.37-29.48-2.09L144 386.59l-10.61 31.88c-5.89 17.66-22.38 29.53-41 29.53H80c-8.84 0-16-7.16-16-16s7.16-16 16-16h12.39c4.83 0 9.11-3.08 10.64-7.66l18.19-54.64c3.3-9.81 12.44-16.41 22.78-16.41s19.48 6.59 22.77 16.41l13.88 41.64c19.75-16.19 54.06-9.7 66 14.16 1.89 3.78 5.49 5.95 9.36 6.26v-82.12l128-127.09V160H248c-13.2 0-24-10.8-24-24V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24v-40l-128-.11c-16.12-.31-30.58-9.28-37.83-23.75zM384 121.9c0-6.3-2.5-12.4-7-16.9L279.1 7c-4.5-4.5-10.6-7-17-7H256v128h128v-6.1zm-96 225.06V416h68.99l161.68-162.78-67.88-67.88L288 346.96zm280.54-179.63l-31.87-31.87c-9.94-9.94-26.07-9.94-36.01 0l-27.25 27.25 67.88 67.88 27.25-27.25c9.95-9.94 9.95-26.07 0-36.01z"/>
                                                         </svg>
@@ -2002,8 +2002,7 @@ module.exports = class MessageLoggerV2 {
     parent.insertBefore(this.channelLogButton, parent.querySelector('div[class*="search-"]'));
   }
   removeOpenLogsButton() {
-    const button = document.getElementById(this.style.openLogs);
-    button.remove();
+    this.channelLogButton.remove();
   }
   showLoggerHelpModal(initial = false) {
     this.createModal({
